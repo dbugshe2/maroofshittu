4778748112ac004f2e94af927c633f093ae6c07d
Maroof Shittu <dbugshe2@gmail.com>
2026-03-20 02:17:13 +0100

    feat: add contact page

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,4 +1,5 @@
 import Image from "next/image";
+import Link from "next/link";
 
 const navLinks = [
   { href: "#", label: "Work" },
@@ -8,26 +9,32 @@ const navLinks = [
 
 export function Navbar() {
   return (
-    <nav className="fixed top-0 left-0 right-0 z-50 backdrop-blur-sm">
-      <div className="container mx-auto px-6 py-4">
-        <div className="flex items-center justify-between">
-          <div className="text-2xl flex gap-2 items-center font-bold text-brand-navy">
-            <Image src="/vercel.svg" alt="Logo" width={32} height={32} />
-            MS
-          </div>
-          <div className="hidden md:flex space-x-8">
-            {navLinks.map((link, idx) => (
-              <a
-                key={`${link.href}-${idx}`}
-                href={link.href}
-                className="text-foreground hover:text-foreground/70 transition-colors"
-              >
-                {link.label}
-              </a>
-            ))}
+    <>
+      <nav className="fixed top-0 left-0 right-0 z-50 h-16 backdrop-blur-sm">
+        <div className="container mx-auto px-6 py-4">
+          <div className="flex items-center justify-between">
+            <Link
+              href="/"
+              className="text-2xl flex gap-2 items-center font-bold text-brand-navy"
+            >
+              <Image src="/logo.svg" alt="Logo" width={32} height={32} />
+              MS
+            </Link>
+            <div className="hidden md:flex space-x-8">
+              {navLinks.map((link, idx) => (
+                <Link
+                  key={`${link.href}-${idx}`}
+                  href={link.href}
+                  className="text-foreground hover:text-foreground/70 transition-colors"
+                >
+                  {link.label}
+                </Link>
+              ))}
+            </div>
           </div>
         </div>
-      </div>
-    </nav>
+      </nav>
+      <div className="h-16 mt-4" />
+    </>
   );
 }
